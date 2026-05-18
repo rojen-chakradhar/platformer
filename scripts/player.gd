47bd1a84143d6da03d63c0592f703e89b34471ff
Rojen Chakradhar <rojenchakradhar@Rojens-MacBook-Air.local>
2026-05-18 20:57:17 +0545

    folder structure and main and player node created

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const SLIDE = 400
 
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +12,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# Handle slide.
+	if Input.is_action_just_pressed("dash"):
+		velocity.x = SPEED + SLIDE
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
